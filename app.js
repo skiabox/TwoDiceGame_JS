420871ffaa15b15839720670f9d1650dabfececa
Stavros Kefaleas <kefaleas.stavros@gmail.com>
2017-03-03 18:01:04 +0200

    Second commit

--- app.js
+++ app.js
@@ -33,33 +33,22 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     {
 
         //1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+
 
         //2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
 
         //3. Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1)
+        if (dice1 !== 1 && dice2 !== 1)
         {
-            //check if this is the first dice
-            if (prevDice === 0)
-                prevDice = dice;
-            else if ( (prevDice === 6) && (dice === 6) )    //check if this is the second dice and if this dice and the previous one are both 6
-            {
-                roundScore = 0;
-                scores[activePlayer] = 0;
-
-                //Update the UI
-                document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-                //next player
-                nextPlayer();
-            }
-
             //Add score
-            roundScore += dice;
+            roundScore += (dice1 + dice2);
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }
         else
@@ -107,7 +96,10 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         else
         {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
@@ -134,7 +126,8 @@ function nextPlayer() {
     //document.querySelector('.player-0-panel').classList.remove('active');
     //document.querySelector('.player-1-panel').classList.add('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 }
 
 
@@ -155,7 +148,9 @@ function init() {
     //v2 rules
     prevDice = 0;
 
-    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
